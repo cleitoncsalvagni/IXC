ab5c46348088be42c61f9747a6c07174bbaa5f92
Cleiton Salvagni <cleitonsalvagni6@gmail.com>
2024-02-15 21:40:44 -0300

    feat(client): head tag

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import Providers from "@/providers";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Providers>
-      <Component {...pageProps} />
-    </Providers>
+    <>
+      <Head>
+        <title>IXChat</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1 maximum-scale=1"
+        />
+        <meta charSet="UTF-8" />
+        <meta name="author" content="Cleiton Salvagni" />
+        <meta name="robots" content="index" />
+      </Head>
+
+      <Providers>
+        <Component {...pageProps} />
+      </Providers>
+    </>
   );
 }
