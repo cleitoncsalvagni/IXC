5e9254a7e0f26f439d1adde8acf5287d7e1a1d16
Cleiton Salvagni <cleitonsalvagni6@gmail.com>
2024-02-18 16:02:49 -0300

    feat(server): native cluster

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 export const connectToDatabase = async (dbUri: string): Promise<void> => {
   try {
     await mongoose.connect(dbUri);
-    console.log("Connected to MongoDB");
   } catch (error: any) {
     console.error("Error connecting to MongoDB", error.message);
   }
